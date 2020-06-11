8bd66194c078618d7ae6e9351a7347368d98b475
Felipe Trova <felipee.trova@gmail.com>
2020-06-11 15:19:21 -0300

    Init configure routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,9 @@
-import express from 'express';
+import express, { request, response } from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-  console.log('listagem de usuarios');
-
-  response.json([
-    "Felipe",
-    "Rodrigo",
-    "Trova",
-    "Opaaa"
-  ]);
-})
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
